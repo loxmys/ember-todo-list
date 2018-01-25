cfaa8bbdcff4b86df1edd27a2f60f6ff3a4b8207
Вадим <mukin.v.i@gmail.com>
2018-01-25 23:43:13 +0300

    add task/new route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('tasks');
+  this.route('tasks', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
