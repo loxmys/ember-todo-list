130be49dbed6198075148dbaf08d8d96052e168c
Вадим <mukin.v.i@gmail.com>
2018-01-25 15:39:47 +0300

    add task model textures

---
+++ mirage/fixtures/tasks.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, title: 'Task_1', description: 'Description 1', priority: 'minor', isCompleted: true },
+  { id: 2, title: 'Task_2', description: 'Description 2', priority: 'critical', isCompleted: false },
+  { id: 3, title: 'Task_3', description: 'Description 3', priority: 'major', isCompleted: false }
+];
