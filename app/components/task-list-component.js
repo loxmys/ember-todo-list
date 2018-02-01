d87dd134e7fd2c08513f7e77f93e252234db6645
Вадим <mukin.v.i@gmail.com>
2018-02-01 16:42:26 +0300

    fix task-list-component

--- app/components/task-list-component.js
+++ app/components/task-list-component.js
@@ -1,22 +1,3 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import $ from 'jquery';
 export default Component.extend({
-  store: service('store'),
-  actions: {
-    deleteTask: function (id) {
-      this.sendAction('deleteTask', id);zxczcx
-    },
-    closeTask: function (id) {
-      this.sendAction('closeTask', id);
-    },
-    reopenTask: function (id) {
-      this.sendAction('reopenTask', id);
-    },
-    dropDownClick: function () {
-      let element = $(event.target);
-      element.siblings('.dropdown-pane').toggleClass('is-open');
-      element.find('.dropdown-pane').toggleClass('is-open');
-    },
-  },
 });
